1a7e4ef10a287f0d10a74b2b2b32a66da328ba00
Cmachado17 <machado.cristian1217@gmail.com>
2020-06-19 20:46:16 -0300

    se agrego cors, ruta para traer los eventos declarados, estado de eventos en front y peticion al back para cargarlos

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,28 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
+const eventos = [
+    {
+        nombre: "Webinar JS",
+        fecha: "19/06/2020",
+    }
+]
+
+//middlewares
+app.use(cors({
+    credentials: true,
+    origin: 'http://localhost:3000',
+    allowedHeaders: ['Content-Type']
+}))
+
+
+//routes
 app.get('/', (req, res)=>{
-    res.send('Hola mundo desde express');
+    res.send(eventos);
 })
 
+//listener
 app.listen('5000', () =>{
     console.log('Conectado a server 5000');
 })
--- frontend/src/components/Calendar.js
+++ frontend/src/components/Calendar.js
@@ -1,10 +1,29 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 
 const localizer = momentLocalizer(moment);
+
 const Calendario = (props) => {
+
+const [eventos, setEventos] = useState([]);
+
+useEffect(() =>{
+  const cargarEventos = async() =>{
+    try{
+      const respuesta = await fetch('http://localhost:5000/')
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+      })
+    }catch(e){
+      alert(e);
+    }
+  }
+  cargarEventos();
+}, [eventos])
+
   const myEventsList = [1, 2, 3, 4];
   return (
     <div className="container">
