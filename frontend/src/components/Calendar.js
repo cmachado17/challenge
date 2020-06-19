3c72dc88dd095d7ff1a102402a6af2d153c8db76
Cmachado17 <machado.cristian1217@gmail.com>
2020-06-18 23:18:23 -0300

    add react-big-calendar, moment.js, react bootstrap

---
+++ frontend/src/components/Calendar.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
+const localizer = momentLocalizer(moment);
+const Calendario = (props) => {
+  const myEventsList = [1, 2, 3, 4];
+  return (
+    <div className="container">
+      <Calendar
+      views={['month', 'week']}
+        localizer={localizer}
+        events={myEventsList}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 500 }}
+      />
+    </div>
+  );
+};
+
+export default Calendario;
