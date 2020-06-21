0e4ebfc2e8a54e0d7514c3b4f5995e916605c2c7
Cmachado17 <machado.cristian1217@gmail.com>
2020-06-21 19:00:04 -0300

    add ruta para consultar los datos de un evento especifico

--- backend/controllers/index.controllers.js
+++ backend/controllers/index.controllers.js
@@ -1,33 +1,49 @@
 let eventos = [
   {
+    id: 1,
     title: "Webinar JS",
     start: "2020-06-19T21:00",
     end: "2020-06-19T22:00",
   },
   {
+    id: 2,
     title: "Webinar Python",
     start: "2020-06-15T15:00",
     end: "2020-06-15T18:00",
   },
   {
+    id: 3,
     title: "React Workshop",
     start: "2020-06-20T12:00",
     end: "2020-06-20T15:00",
   },
   {
+    id: 4,
     title: "Angular Workshop",
     start: "2020-06-19T21:00",
     end: "2020-06-19T22:00",
   },
-
 ];
-
+//traer todos los eventos
 const getEvents = async (req, res) => {
   await res.send(eventos);
 };
 
+//traer evento especifico
+const getEvent = (req, res) => {
+  try {
+    eventos.map((evento) => {
+      if (evento.id == req.params.id) {
+        res.send(evento);
+      }
+    });
+  } catch (e) {
+    console.log(e);
+  }
+};
+//crear nuevo evento
 const newEvent = async (req, res) => {
-console.log(req.body)
+  console.log(req.body);
   try {
     const { titulo, inicio, final } = req.body;
     const eventInsert = {
@@ -37,8 +53,8 @@ console.log(req.body)
     };
     eventos.push(eventInsert);
     res.send({
-        status: "ok",
-        message: "Evento creado satisfactoriamente"
+      status: "ok",
+      message: "Evento creado satisfactoriamente",
     });
   } catch (e) {
     console.log(e);
@@ -63,5 +79,6 @@ console.log(req.body)
 
 module.exports = {
   getEvents,
+  getEvent,
   newEvent,
 };
--- backend/routers/index.js
+++ backend/routers/index.js
@@ -3,9 +3,11 @@ const router = Router();
 const multer  = require('multer')
 const upload = multer({ dest: 'uploads/' })
 
-const {getEvents, newEvent} = require('../controllers/index.controllers');
+const {getEvents, newEvent, getEvent} = require('../controllers/index.controllers');
 
-router.get('/get-events',upload.fields([]), getEvents);
+router.get('/get-events', getEvents);
+router.get('/evento/:id', getEvent);
 router.post('/new-event',upload.fields([]), newEvent);
+
 
 module.exports = router;
